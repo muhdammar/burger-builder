a51f56e689a9521c4e485ebadd8065aa33d051ea
Unknown <muhdammar970320@gmail.com>
2018-07-23 10:43:10 +0800

    update

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,9 +1,21 @@
 import Aux from '../../hoc/Aux';
 import React from 'react';
-
+import classes from '../Burger/Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = ( props ) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey=> {
+        return [...Array(props.ingredients[igKey])].map((__, i)=> {
+            console.log(igKey);
+           return <BurgerIngredient key= {igKey + i} type = {igKey} />
+        });
+    });
     return (
-
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
     );   
 };
 
